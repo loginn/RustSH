4da6e686799a3350a0b077bfe04d47cfa571bd1a
Vincent Kurta <vincent@swapcard.com>
2018-05-04 18:54:41 +0100

    nique

--- src/cd.rs
+++ src/cd.rs
@@ -1,21 +1,43 @@
 use std::env;
 use std::path;
 
-fn change_directory(path: path::PathBuf) {
-    match env::set_current_dir(&path) {
-        Err(err) => println!("Error : {}", err),
-        _ => {}
+fn change_directory(path: &path::PathBuf) {
+    match env::set_current_dir(path) {
+        Err(err) => println!("Error : {}, {}", err, path.to_str().unwrap()),
+        Ok(_) => println!("Success : {}", path.to_str().unwrap())
     }
+}
+
+fn build_new_path(command_path: &str) -> Option<path::PathBuf> {
+    let mut desired_path = path::PathBuf::new();
+    let user_path = path::PathBuf::from(command_path);
+    if !user_path.is_absolute() {
+        let current_path = env::current_dir();
+        match current_path {
+            Ok(n) => {
+                desired_path.push(n);
+                desired_path.push(user_path);
+            },
+            Err(e) => {
+                println!("Error : {}", e);
+                return None;
+            }
+        }
+    } else {
+        desired_path.push(user_path);
+    }
+    return Some(desired_path)
 }
 
 pub fn cd (command_vector: &Vec<&str> ) {
     if command_vector.len() == 2 {
-        let mut new_path = path::PathBuf::new();
-        new_path.push(command_vector[1]);
-        change_directory(new_path);
+        match build_new_path(&command_vector[1]) {
+            Some(n) => change_directory(&n),
+            None => return
+        }
     } else if command_vector.len() == 1 {
         match env::home_dir() {
-            Some(n) => change_directory(n),
+            Some(n) => change_directory(&n),
             None => println!("No known home directory")
         }
     } else {
