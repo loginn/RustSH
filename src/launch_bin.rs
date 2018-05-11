74585a22fee1859138f2ce84798bce6dff965be4
Vincent Kurta <vincent@swapcard.com>
2018-05-11 10:53:24 +0100

    clearer errors

--- src/launch_bin.rs
+++ src/launch_bin.rs
@@ -2,15 +2,16 @@ use std::process::Command;
 
 pub fn launch_bin(command_vector: &mut Vec<&str>)
 {
-    let mut process = Command::new(command_vector[0].trim());
+    let program_string = command_vector[0];
+    let mut process = Command::new(program_string.trim());
 
     command_vector.remove(0);
     for val in command_vector {
-        process.arg(val.trim());;
+        process.arg(val.trim());
     }
     let child = process.spawn().ok();
     if child.is_none() {
-        print!("Couldn't launch process")
+        println!("Couldn't launch process: {}", program_string)
     } else {
         child.unwrap().wait().unwrap();
     }
